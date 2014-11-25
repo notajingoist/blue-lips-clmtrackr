5313a854912f8f69426bfdc2e578e46a9d1e6e75
Jing Xiao <notajingoist@gmail.com>
2014-11-25 17:20:24 -0500

    added emotion bars

--- public/js/src/scripts.js
+++ public/js/src/scripts.js
@@ -1,3 +1,5 @@
+
+
 var BLUELIPS = {
     init: function() {
         this.initVars();
@@ -6,6 +8,7 @@ var BLUELIPS = {
         this.setupVideo();
         this.initClm();
         this.initEmotion();
+        this.initd3();
     },
 
     initVars: function() {
@@ -96,6 +99,13 @@ var BLUELIPS = {
         var er = this.ec.meanPredict(cparam);
         if (er) {
             this.updateData(er);
+            for (var i = 0;i < er.length;i++) {
+                if (er[i].value > 0.4) {
+                    document.getElementById('icon'+(i+1)).style.visibility = 'visible';
+                } else {
+                    document.getElementById('icon'+(i+1)).style.visibility = 'hidden';
+                }
+            }
         }
     },
 
@@ -106,15 +116,93 @@ var BLUELIPS = {
     },
 
     updateData: function(data) {
-        for (var i = 0; i < data.length; i++) {
-            // console.log(data[i]);
-        }
+        // for (var i = 0; i < data.length; i++) {
+        //     console.log(data[i]);
+        // }
+
+        // update
+        var context = this;
+        var rects = this.svg.selectAll("rect")
+            .data(data)
+            .attr("y", function(datum) { return context.height - context.y(datum.value); })
+            .attr("height", function(datum) { return context.y(datum.value); });
+        var texts = this.svg.selectAll("text.labels")
+            .data(data)
+            .attr("y", function(datum) { return context.height - context.y(datum.value); })
+            .text(function(datum) { return datum.value.toFixed(1);});
+        // enter
+        rects.enter().append("svg:rect");
+        texts.enter().append("svg:text");
+        // exit
+        rects.exit().remove();
+        texts.exit().remove();
     },
 
     updatePos: function(positions) {
         for (var i = 0; i < positions.length; i++) {
             // console.log(positions[i]);
         }
+    },
+
+    initd3: function() {
+        var margin = {top : 20, right : 20, bottom : 10, left : 40},
+            width = 400 - margin.left - margin.right,
+            height = 100 - margin.top - margin.bottom;
+        var barWidth = 30;
+        var formatPercent = d3.format(".0%");
+
+        this.margin = margin;
+        this.width = width;
+        this.height = height;
+        this.barWidth = barWidth;
+        this.formatPercent = formatPercent;
+
+        var x = d3.scale.linear()
+            .domain([0, this.ec.getEmotions().length]).range([margin.left, width+margin.left]);
+        var y = d3.scale.linear()
+            .domain([0,1]).range([0, height]);
+
+        this.x = x;
+        this.y = y;
+
+        this.svg = d3.select("#emotion-chart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+
+        this.svg.selectAll("rect").
+            data(this.emotionData).
+            enter().
+            append("svg:rect").
+            attr("x", function(datum, index) { return x(index) - barWidth; }).
+            attr("y", function(datum) { return height - y(datum.value); }).
+            attr("height", function(datum) { return y(datum.value); }).
+            attr("width", barWidth).
+            attr("fill", "#2d578b");
+
+        this.svg.selectAll("text.labels").
+            data(this.emotionData).
+            enter().
+            append("svg:text").
+            attr("x", function(datum, index) { return x(index); }).
+            attr("y", function(datum) { return height - y(datum.value); }).
+            attr("dx", -barWidth/2).
+            attr("dy", "1.2em").
+            attr("text-anchor", "middle").
+            text(function(datum) { return datum.value;}).
+            attr("fill", "white").
+            attr("class", "labels");
+
+        this.svg.selectAll("text.yAxis").
+            data(this.emotionData).
+            enter().append("svg:text").
+            attr("x", function(datum, index) { return x(index); }).
+            attr("y", height + 7).
+            attr("dx", -barWidth/2).
+            attr("text-anchor", "middle").
+            attr("style", "font-size: 13px").
+            text(function(datum) { return datum.emotion;}).
+            attr("transform", "translate(0, 18)").
+            attr("class", "yAxis");
     }
 
 }
