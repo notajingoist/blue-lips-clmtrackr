300712081b16593e6f28bba955cd73334c0da7ff
Jing Xiao <notajingoist@gmail.com>
2014-11-25 14:50:37 -0500

    added emotion detector face tracking

--- public/js/src/scripts.js
+++ public/js/src/scripts.js
@@ -3,10 +3,22 @@ var BLUELIPS = {
         this.initVars();
         this.initPlugins();
         this.bindEventHandlers();
+        this.setupVideo();
+        this.initClm();
+        this.initEmotion();
     },
 
     initVars: function() {
+        this.$vid = $('#video-el');
+        this.vid = this.$vid[0];
 
+        this.$overlay = $('#overlay');
+        this.overlay = this.$overlay[0];
+
+        this.overlayCC = this.overlay.getContext('2d');
+
+        this.$startBtn = $('#start-button');
+        this.startBtn = this.$startBtn[0]
     },
 
     initPlugins: function() {
@@ -14,8 +26,96 @@ var BLUELIPS = {
     },
 
     bindEventHandlers: function() {
+        this.$startBtn.on('click', this.startVideo.bind(this));
+    },
 
+    setupVideo: function() {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+        window.URL = window.URL || window.webkitURL || window.msURL || window.mozURL;
+
+        // check for camerasupport
+        if (navigator.getUserMedia) {
+            // set up stream
+            var videoSelector = {video : true};
+            if (window.navigator.appVersion.match(/Chrome\/(.*?) /)) {
+                var chromeVersion = parseInt(window.navigator.appVersion.match(/Chrome\/(\d+)\./)[1], 10);
+                if (chromeVersion < 20) {
+                    videoSelector = "video";
+                }
+            };
+
+            var context = this;
+            navigator.getUserMedia(videoSelector, function( stream ) {
+                if (context.vid.mozCaptureStream) {
+                    context.vid.mozSrcObject = stream;
+                } else {
+                    context.vid.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+                }
+                context.vid.play();
+            }, function() {
+                //insertAltVideo(vid);
+                alert("There was some problem trying to fetch video from your webcam. If you have a webcam, please make sure to accept when the browser asks for access to your webcam.");
+            });
+        } else {
+            //insertAltVideo(vid);
+            alert("This demo depends on getUserMedia, which your browser does not seem to support. :(");
+        }
+
+        this.$vid.on('canplay', this.enableStart.bind(this));
+    },
+
+    enableStart: function() {
+        this.$startBtn.attr('value', 'start');
+        this.$startBtn.attr('disabled', null);
+    },
+
+    initClm: function() {
+        this.ctrack = new clm.tracker({useWebGL: true});
+        this.ctrack.init(pModel);
+    },
+
+    startVideo: function() {
+        //start video
+        this.vid.play();
+
+        //start tracking
+        this.ctrack.start(this.vid);
+
+        //start loop to draw face
+        this.drawLoop();
+    },
+
+    drawLoop: function() {
+        requestAnimFrame(this.drawLoop.bind(this));
+        this.overlayCC.clearRect(0, 0, 400, 300);
+        if (this.ctrack.getCurrentPosition()) {
+            this.ctrack.draw(this.overlay);
+        }
+        var cp = this.ctrack.getCurrentParameters();
+
+        var er = this.ec.meanPredict(cp);
+        if (er) {
+            this.updateData(er);
+            // for (var i = 0;i < er.length;i++) {
+            //     if (er[i].value > 0.4) {
+            //         document.getElementById('icon'+(i+1)).style.visibility = 'visible';
+            //     } else {
+            //         document.getElementById('icon'+(i+1)).style.visibility = 'hidden';
+            //     }
+            // }
+        }
+    },
+
+    initEmotion: function() {
+        this.ec = new emotionClassifier();
+        this.ec.init(emotionModel);
+        this.emotionData = this.ec.getBlank();
+    },
+
+    updateData: function(er) {
+        console.log(er[0]);
     }
+
 }
 
 BLUELIPS.init();
